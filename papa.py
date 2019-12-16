e5e32724f491b65b94d82f11e8f175cf0d778349
marcos <marcosazcona2000@hotmail.com>
2019-12-16 12:08:19 -0300

    Agregado la conversion de un audio

--- papa.py
+++ papa.py
@@ -1,26 +1,15 @@
+import os
+import json
+import time
+import PySimpleGUI as sg
 from pydub import AudioSegment
 import speech_recognition as sr
-import json
-import os
-import time
-lista=list()
-def guardar_salida(salida):
-    if os.stat('archivo.json').st_size == 0:
-         with open('archivo.json','w',encoding='utf-8') as file:
-            lista.append(salida)
-            json.dump(lista,file)
-    else:
-        with open('archivo.json','r',encoding='utf-8') as file:
-            contenido=json.load(file)
-            contenido[0]=contenido[0]+' '+salida
-        with open('archivo.json','w',encoding='utf-8') as file:
-            json.dump(contenido,file)
 
-def leer_lo_guardado():
-    with open('archivo.json','r',encoding='utf-8') as file:
-        print(json.load(file))
 
 def speech_to_text():
+    """
+        Este es el módulo que anda bien
+    """
     audios=['audio1.wav','audio2.wav','audio3.wav']
     r=sr.Recognizer() #Declaro el objeto reconocedor120
     r.energy_threshold = 4000
@@ -43,32 +32,62 @@ def speech_to_text():
                 duration_value=120
                 offset_value=179
 
-def get_iterations(segundos):
-    i=1
-    while segundos > 0:
-        segundosPrueba-=180
-        i+=1
-    return i
 
-def speech_to_textV2(audio,duration):
-    """
-        This function returns a string of the audio converted into string.
-    """
-    seconds = (duration * 60)
-    iterations=get_iterations(second)
-    string=''
-    r = sr.Recognizer()
-    r.energy_threshold=4000
-    offset_value=0
-    duration_value=180
-    r.adjust_for_ambient_noise(source)        
-    for i in iterations:
-        with audio as source:
-            r.record(source,offset=offset_value,duration=duration_value) #Grabo el archivo wav
-            output=r.recognize_google(audio,show_all=True,language='es-ES') #Lo paso por el recognizer de google
-            string=string+' '+output #Concateno la salida con lo que ya tenía en el string
-            offset_value+=180 #Me muevo 180 segundos más del audio, para arrancar de donde quedé 
-    return string
+def speech_to_text3():
+    "Convert only one audio"
+    salida=''
+    r=sr.Recognizer() #Declaro el objeto reconocedor120
+    r.energy_threshold = 4000
+    duration_value=180 #Duration es cuánto grabar del audio
+    offset_value=0 #Offset es desde dónde arrancar el audio
+    demo=sr.AudioFile('audio4.wav') #Le paso el audio
+    with demo as source:
+        r.adjust_for_ambient_noise(source) #Le ajusto el sonido ambiente
 
-    
-leer_lo_guardado()
+        print('Valor offset ',offset_value)
+        print('Valor duracion',duration_value)
+        audio=r.record(source,offset=179,duration=166) #Lo paso a la variable audio 
+                    
+        output=r.recognize_google(audio,show_all=True, language="es-ES") 
+        salida=salida+' '+(output['alternative'][0]['transcript'])
+        print(salida)
+        print(salida)
+
+
+
+def get_extension(string):
+    fileToConvert= (string.split('/'))
+    fileToConvert=fileToConvert[len(fileToConvert)-1]
+    return ((os.path.splitext(fileToConvert)[1])[1:])
+
+def convert_audio_to_mp3(audio,location):
+    extension=get_extension(audio)
+    audio = AudioSegment.from_file(audio,format=extension)
+    location=location+'/'+'archivoNuevo.mp3'
+    audio.export(location,format='mp3')
+    audio.export('audioWav.wav',format='wav')
+
+
+#print(os.getcwd())
+layout = [
+            [sg.Text('Elija el archivo a convertir')],
+            [sg.Input(), sg.FileBrowse()],
+            [sg.Text('Elija donde guardar')],
+            [sg.Input(),sg.FolderBrowse()],
+            [sg.Submit('Confirmar'),sg.Cancel('Cancelar')]
+
+        ]
+window = sg.Window('Ventana',size=(500,500)).layout(layout)
+
+while True:
+    event,values = window.Read()
+    print('Evento {}'.format(event))
+    print('Valores {}'.format(values))
+    if(event == 'Confirmar' and values['Browse'] != '' and values['Browse0'] != ''):
+        print('Entre ',values['Browse'])
+        location = values['Browse0']
+        convert_audio_to_mp3(values['Browse'],location)
+        window.Close()
+        break
+    elif(event == None):
+        break
